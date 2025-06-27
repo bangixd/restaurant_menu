b2b9b44198748b7e53e273a963debe20807747a1
jay <abolfazls4yy4h@gmail.com>
2025-06-28 02:32:47 +0430

    [ADD] add models to restaurant for restaurant category and menu item and make migrate

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Restaurant(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='restaurants')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    address = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='restaurant_logos/', null=True, blank=True)
+    slug = models.SlugField(unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuCategory(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='categories')
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.restaurant.name} - {self.title}"
+
+
+class MenuItem(models.Model):
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, related_name='items')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='menu_items/', null=True, blank=True)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
